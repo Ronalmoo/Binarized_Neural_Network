f938139b14b334763f98ad12dd88484e2a83fdc5
Ronalmoo <ydaniel0826@gmail.com>
2019-11-22 18:39:51 +0900

    add raise Error message

--- utils/layer.py
+++ utils/layer.py
@@ -19,14 +19,14 @@ def binarization(w: torch.tensor, mode="deterministic") -> torch.tensor:
             return stochastic(w)
 
         else:
-            raise RuntimeError
+            raise RuntimeError("mode name should be 'deterministic' or 'stochastic'")
 
 
 def deterministic(w: torch.tensor) -> torch.tensor:
     with torch.no_grad():
         w[w >= 0] = 1
         w[w < 0] = -1
-    return w
+        return w
 
 
 def stochastic(w: torch.tensor) -> torch.tensor:
@@ -35,7 +35,7 @@ def stochastic(w: torch.tensor) -> torch.tensor:
         matrix = torch.empty(p.shape).uniform_(-1, 1)
         bin_weight = (p >= matrix).type(torch.float32)
         bin_weight[bin_weight == 0] = -1
-    return bin_weight
+        return bin_weight
 
 
 if __name__ == "__main__":
@@ -64,7 +64,7 @@ if __name__ == "__main__":
     print("example: {}".format(example_grad_true))
     prob = hard_sigmoid(example_grad_true)
     print("prob: {}".format(prob))
-    binarization_det = binarization(example, mode='deterministic')
+    binarization_det = binarization(example_grad_true, mode='deterministic')
 
     print("Binarization: {}".format(binarization_det))
 
@@ -74,3 +74,4 @@ if __name__ == "__main__":
     for i in range(99):
         binarization_sum += binarization(example_grad_true, mode="stochastic")
     print("After sum Binarizaion: {}".format(binarization_sum))
+
