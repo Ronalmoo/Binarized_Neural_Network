66fa872dde510b67eb48350927fa2cd73341cf24
Ronalmoo <ydaniel0826@gmail.com>
2019-11-29 23:21:32 +0900

    add BinaryLinear

--- utils/layer.py
+++ utils/layer.py
@@ -5,21 +5,34 @@ import numpy as np
 from typing import *
 
 
-def hard_sigmoid(x: torch.tensor) -> torch.tensor:
-    clamp = (torch.clamp((x + 1.0) / 2.0, 0, 1))
-    return clamp
+class BinaryLinear(nn.Linear):
+    def __init__(self, in_features, out_features, mode='Stochastic'):
+        super(BinaryLinear, self).__init__(in_features, out_features)
+        self.mode = mode
+        self.bias = nn.Parameter(torch.randn(in_features, out_features))
+        self.bin_weight = self.binarization(self.weight, self.mode)
 
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        self.clipping_weight(self.weight)
+        self.bin_weight = self.binarization(self.weight, self.mode)
+        return F.linear(x, self.bin_weight, self.bias)
 
-def binarization(w: torch.tensor, mode="deterministic") -> torch.tensor:
-    with torch.no_grad():
-        if mode == 'deterministic':
-            return deterministic(w)
+    def binarization(self, w: torch.tensor, mode: str) -> torch.tensor:
+        with torch.no_grad():
+            if mode == 'deterministic':
+                bin_weight = deterministic(w)
 
-        elif mode == "stochastic":
-            return stochastic(w)
+            elif mode == "stochastic":
+                bin_weight = stochastic(w)
 
-        else:
-            raise RuntimeError("mode name should be 'deterministic' or 'stochastic'")
+            else:
+                raise RuntimeError("mode name should be 'deterministic' or 'stochastic'")
+        bin_weight.requires_grad = True
+        bin_weight.register_hook(self.grad_deep_copy)
+        return bin_weight
+
+    def grad_deep_copy(self, grad):
+        self.weight.grad = torch.detach(grad).clone()
 
 
 def deterministic(w: torch.tensor) -> torch.tensor:
@@ -38,40 +51,11 @@ def stochastic(w: torch.tensor) -> torch.tensor:
         return bin_weight
 
 
+def hard_sigmoid(x: torch.tensor) -> torch.tensor:
+    clamp = (torch.clamp((x + 1.0) / 2.0, 0, 1))
+    return clamp
+
+
 if __name__ == "__main__":
-    # Test code
-    print("torch_with_no_grad\n")
-    example = torch.randn(5, 5)
-    print("example: {}".format(example))
-    prob = hard_sigmoid(example)
-    print("prob: {}".format(prob))
-    binarization_det = binarization(example, mode='deterministic')
-
-    print("Binarization: {}".format(binarization_det))
-
-    binarization_sum = binarization(example, mode='stochastic')
-    print("Before sum Binarization: {}".format(binarization_sum))
-
-    for i in range(99):
-        binarization_sum += binarization(example, mode="stochastic")
-    print("After sum Binarizaion: {}".format(binarization_sum))
-
-    print("\n------------------------------------------------------------\n" * 2)
-    print("torch_with_grad\n")
-
-    example_grad_true = torch.rand(5, 5, requires_grad=True)
-    print(example_grad_true)
-    print("example: {}".format(example_grad_true))
-    prob = hard_sigmoid(example_grad_true)
-    print("prob: {}".format(prob))
-    binarization_det = binarization(example_grad_true, mode='deterministic')
-
-    print("Binarization: {}".format(binarization_det))
-
-    binarization_sum = binarization(example_grad_true, mode='stochastic')
-    print("Before sum Binarization: {}".format(binarization_sum))
-
-    for i in range(99):
-        binarization_sum += binarization(example_grad_true, mode="stochastic")
-    print("After sum Binarizaion: {}".format(binarization_sum))
-
+    binary_linear = BinaryLinear(3, 3, 'stochastic')
+    print(binary_linear)
